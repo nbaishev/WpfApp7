a1a396fc5f0369e089094bc51c7c22ccf327abcb
Николай Баишев <baishev.nk@stud.s-vfu.ru>
2022-03-11 17:25:21 +0900

    with 8 task

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace WpfApp3
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            string fontName = (sender as ComboBox).SelectedItem as String;
             if (textBox != null)
             {
                 textBox.FontFamily = new FontFamily(fontName);
@@ -38,7 +38,7 @@ namespace WpfApp3
 
         private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
-            string fontSize = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            string fontSize = (sender as ComboBox).SelectedItem as String;
             if (textBox != null)
             {
                 textBox.FontSize = Convert.ToDouble(fontSize);
